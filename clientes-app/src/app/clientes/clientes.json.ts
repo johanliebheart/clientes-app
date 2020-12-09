77487aea12510729e3b0ed09518e380d790603f3
Erick González <itenhuck@gmail.com>
2020-12-09 01:05:37 -0600

    Base de proyecto

---
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Erick', apellido: 'González', email: 'hola@mail.com', createAt: '2020-04-02' },
+  { id: 2, nombre: 'Andres', apellido: 'Escalera', email: 'hola1@mail.com', createAt: '2020-04-01' },
+  { id: 3, nombre: 'Juan', apellido: 'Puerta', email: 'hola2@mail.com', createAt: '2020-04-03' },
+  { id: 4, nombre: 'Laura', apellido: 'Aguilar', email: 'hola3@mail.com', createAt: '2020-04-04' },
+  { id: 5, nombre: 'Ana', apellido: 'Espinoza', email: 'hola4@mail.com', createAt: '2020-04-05' },
+  { id: 6, nombre: 'Agustin', apellido: 'Iturbide', email: 'hola5@mail.com', createAt: '2020-04-06' },
+  { id: 7, nombre: 'Javier', apellido: 'Perez', email: 'hola6@mail.com', createAt: '2020-04-05' },
+  { id: 8, nombre: 'Maria', apellido: 'Ramirez', email: 'hola7@mail.com', createAt: '2020-04-07' },
+  { id: 9, nombre: 'Claudia', apellido: 'Gomez', email: 'hola8@mail.com', createAt: '2020-04-08' }
+];
