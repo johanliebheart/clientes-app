2f0185d53b8289b62d29e9f04d0c69bbbebae54a
Erick González <itenhuck@gmail.com>
2020-12-10 18:12:52 -0600

    Update

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -13,7 +14,9 @@ export class ClientesComponent implements OnInit {
   constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
-    this.clientes = this.clienteService.getClientes();
+
+    this.clienteService.getClientes().subscribe(clientes => this.clientes = clientes);
+
   }
 
 }
