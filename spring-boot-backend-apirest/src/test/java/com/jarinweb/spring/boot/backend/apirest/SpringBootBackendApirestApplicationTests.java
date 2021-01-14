532607a9f614062128f83dec89e71f86342b1ea9
Erick González <itenhuck@gmail.com>
2021-01-14 16:55:17 -0600

    Creación de proyecto Spring

---
+++ spring-boot-backend-apirest/src/test/java/com/jarinweb/spring/boot/backend/apirest/SpringBootBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarinweb.spring.boot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
